32faacab79980d4364addfe36d9f9537a087efe6
Programming-NMCOTyet <162403457+Programming-NMCOTyet@users.noreply.github.com>
2024-05-04 02:01:32 -0500

    Update daniel.cc

--- daniel.cc
+++ daniel.cc
@@ -1,204 +1,215 @@
 #include <iostream>
-#include <malloc.h>  
+#include <malloc.h> 
 using namespace std;
 
+
 struct Nodo {
-    int codigo;
-    char nombre[50];
-    int fechaven;
-    char dia[30];
-    char mes[30];
-    Nodo* izq;
-    Nodo* der;
+    int codigo; 
+    char nombreEmpr[50];
+    int fechaven; 
+    Nodo* izq; 
+    Nodo* der; 
 };
 
-Nodo* raiz = nullptr, *aux,*aux2;
+
+Nodo* raiz = nullptr, *aux;
 Nodo* subraiz = nullptr;
 
 
-void preorden(Nodo* aux3) {
-    if (aux3 != nullptr) {
+int obtenerFecha() {
 
-        cout << "su codigo es :" << " - "<<aux3->codigo<<endl;
-        cout<<"su nombre es :"<< " - " << aux3->nombre << endl;  
-        cout<<"su fecha de vencimiento es :"<< " - "  << aux3->fechaven << " - " << aux3->mes << " - " << aux3->dia <<endl;
-        
-        preorden(aux3->izq);
-        preorden(aux3->der);
+    int year, mes, dia;
+
+    cout << "Ingrese el año (4 dígitos): ";
+    cin >> year;
+
+    cout << "Ingrese el mes (1-12): ";
+    cin >> mes;
+
+    cout << "Ingrese el día (1-31): ";
+    cin >> dia;
+
+    
+    if (mes < 1 || mes > 12 || dia < 1 || dia > 31) {
+        cout << "Fecha no válida. Inténtelo nuevamente." << endl;
+        return obtenerFecha(); 
+    }
+
+    
+    int fecha = year * 10000 + mes * 100 + dia; 
+    return fecha;
+}
+
+
+void posicionar() {
+
+    Nodo* actual = raiz;
+
+    while (true) { 
+
+        if (aux->codigo < actual->codigo) { 
+            if (actual->izq == nullptr) {
+                actual->izq = aux;
+               break;
+            }
+            actual = actual->izq; 
+        } else {
+            if (actual->der == nullptr) {
+                actual->der = aux;
+              break;
+            }
+            actual = actual->der; 
+        }
     }
 }
+
+
+void posicionarfe() {
+
+    Nodo* actual = subraiz; 
+    while (actual != nullptr) { 
+        if (aux->fechaven < actual->fechaven) {
+            if (actual->izq == nullptr) {
+                actual->izq = aux;
+                break;
+            }
+            actual = actual->izq; 
+        } else {
+            if (actual->der == nullptr) {
+                actual->der = aux;
+               break;
+            }
+            actual = actual->der; 
+        }
+    }
+}
+
+
+void registrar() {
+
+    aux = (Nodo*)malloc(sizeof(Nodo)); 
+
+    cout << "Ingrese el nombre de la empresa: ";
+    cin >> aux->nombreEmpr;
+
+    cout << "Ingrese su código (RNT): ";
+    cin >> aux->codigo; 
+
+    aux->fechaven = obtenerFecha();
+
+    aux->izq = aux->der = nullptr;
+
+   
+    if (raiz == nullptr) {
+        raiz = aux; 
+    } else {
+        posicionar(); 
+    }
+
+    
+    if (subraiz == nullptr) {
+        subraiz = aux;
+    } else {
+        posicionarfe(); 
+    }
+}
+
+
+void preorden(Nodo* aux3) {
+
+    if (aux3 != nullptr) {
+
+        cout << "Código: " << aux3->codigo << ", Nombre de empresa: " << aux3->nombreEmpr << ", Fecha de vencimiento: " << aux3->fechaven <<endl<< endl;
+        preorden(aux3->izq); 
+        preorden(aux3->der); 
+    }
+}
+
 
 void inorden(Nodo* aux3) {
 
     if (aux3 != nullptr) {
 
         inorden(aux3->izq);
-
-        cout << "su codigo es :" << " - "<<aux3->codigo<<endl;
-        cout<<"su nombre es :"<< " - " << aux3->nombre << endl; 
-        cout<<"su fecha de vencimiento es :"<< " - "  << aux3->fechaven << " - " << aux3->mes << " - " << aux3->dia <<endl;
-
-        inorden(aux3->der);
+        cout << "Código: " << aux3->codigo << ", Nombre de empresa: " << aux3->nombreEmpr << ", Fecha de vencimiento: " << aux3->fechaven << endl<<endl;    
+        inorden(aux3->der); 
     }
 }
 
-void postorden(Nodo* aux3) {
+
+void postorden (Nodo* aux3) {
+
     if (aux3 != nullptr) {
 
-        postorden(aux3->izq);
-        postorden(aux3->der);
-
-        cout << "su codigo es :" << " - "<<aux3->codigo<<endl;
-        cout<<"su nombre es :"<< " - " << aux3->nombre << endl;
-        cout<<"su fecha de vencimiento es :"<< " - "  << aux3->fechaven << " - " << aux3->mes << " - " << aux3->dia <<endl;
-
+        postorden(aux3->izq); 
+        postorden(aux3->der); 
+        cout << "Código: " << aux3->codigo << ", Nombre de empresa: " << aux3->nombreEmpr << ", Fecha de vencimiento: " << aux3->fechaven << endl<< endl;
+      
     }
-}
-
-void posicionar() {
-
-    while (aux2 != nullptr) {
-        if (aux->codigo < aux2->codigo) {
-            if (aux2->izq == nullptr) {
-                aux2->izq = aux;
-                
-            }
-            aux2 = aux2->izq;
-        } else {
-            if (aux2->der == nullptr) {
-                aux2->der = aux;
-                
-            }
-            aux2 = aux2->der;
-        }
-    }
-}
-
-void posicionarfe() {
-
-    aux2 = subraiz;  
-
-    while (aux2 != nullptr) {
-        if (aux->fechaven < aux2->fechaven) {
-            if (aux2->izq == nullptr) {
-                aux2->izq = aux;
-               
-            }
-            aux2 = aux2->izq;
-        } else {
-            if (aux2->der == nullptr) {
-                aux2->der = aux;
-                
-            }
-            aux2 = aux2->der;
-        }
-    }
-}
-
-void registrar(){
-
-    aux = (Nodo*)malloc(sizeof(Nodo));
-
-    cout << "Ingrese su nombre: ";
-    cin >> aux->nombre;
-
-    cout << "Ingrese su codigo: ";
-    cin >> aux->codigo;
-
-    cout << "Ingrese su fecha de vencimiento: ";
-    cin >> aux->fechaven;
-
-    cout << "Ingrese el mes: ";
-    cin >> aux->mes;
-
-    cout << "Ingrese el día: ";
-    cin >> aux->dia;
-
-    aux->izq = aux->der = nullptr;
-
-    if (raiz == nullptr) {
-        raiz = aux;  
-    } else {
-        aux2 = raiz;
-        posicionar();
-    }
-
-    if (subraiz == nullptr) {
-        subraiz = aux;  
-    } else {
-        aux2 = subraiz;
-        posicionarfe();
-    }
-
-    aux = nullptr; 
 }
 
 int main() {
     int op;
-
     do {
-        cout << "*****************" << endl;
 
-        cout << "MENU" << endl;
+        cout << "********************************************************" << endl;
+        cout << " MENU DE REGISTRO E INFORMACION" << endl<<endl;
         
         cout << "1. Registrar usuario" << endl;
-        cout << "2. Preorden del árbol principal" << endl;
-        cout << "3. Inorden del árbol principal" << endl;
-        cout << "4. Postorden del árbol principal" << endl;
-        cout << "5. Preorden del subárbol" << endl;
-        cout << "6. Inorden del subárbol" << endl;
-        cout << "7. Postorden del subárbol" << endl;
-        cout << "8. Eiminar codigo"<<endl;
 
-        cout << "*****************" << endl;
-      
+        cout << "2. Preorden del árbol principal" << endl;
+        cout << "3. In orden del árbol principal" << endl;
+        cout << "4. Post orden del árbol principal" << endl;
+
+        cout << "5. Preorden del subárbol" << endl;
+        cout << "6. In orden del subárbol" << endl;
+        cout << "7. Post orden del subárbol" << endl;
+
+        cout << "8. Salir" << endl;
+
+        cout << "Seleccione una opción: "<<endl<<endl;
+        cout << "********************************************************" << endl;
         cin >> op;
 
         switch (op) {
+
             case 1:
-                registrar();
+                registrar(); 
                 break;
 
             case 2:
-
-            cout << "LISTADO DE REGISTRO.."<<endl<<endl;
-                preorden(raiz);
-                break;
-
+                cout << "Listado de registro Preorden del árbol principal:" <<endl<< endl;
+                preorden(raiz); 
+            break;
             case 3:
-             cout << "LISTADO DE REGISTRO.."<<endl<<endl;
-                inorden(raiz);
-                break;
-
+                cout << "Listado de registro Inorden del árbol principal:" <<endl<< endl;
+                inorden(raiz); 
+            break;
             case 4:
-             cout << "LISTADO DE REGISTRO.."<<endl<<endl;
-                postorden(raiz);
-                break;
-
+                cout << "Listado de registro Post orden del árbol principal:" <<endl<<endl;
+                postorden(raiz); 
+            break;
             case 5:
-              cout << "LISTADO DE REGISTRO.."<<endl<<endl;
+                cout << "Listado de registro Preorden del árbol segundario:" <<endl<< endl;
                 preorden(subraiz); 
-                break;
-
+            break;
             case 6:
-             cout << "LISTADO DE REGISTRO.."<<endl<<endl;
-                inorden(subraiz);  
-                break;
-
+                cout << "Listado de registro Preorden del árbol segundario:" << endl<<endl;
+                inorden(subraiz); 
+            break;
             case 7:
-             cout << "LISTADO DE REGISTRO.."<<endl<<endl;
-                postorden(subraiz);  
-                break;
-
+                cout << "Listado de registro Preorden del árbol segundario:" << endl<< endl;
+                postorden(subraiz); 
+            break;
             case 8:
-                
-                break;
-            
+                cout << "Saliendo..." << endl;
+            break;
             default:
-                cout << "Opción inválida. Elige un número entre 1 y 8." << endl;
+                cout << "Opción no válida vro." << endl;
+            break;
         }
-
     } while (op != 8);
 
-    return 0;
+    return 0; 
 }
+
